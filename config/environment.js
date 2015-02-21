d0ef67d4e552b7e6df98acc96afa8a2e4b523fd3
Karel Vuong <vuongkarel@gmail.com>
2015-02-20 23:58:57 -0800

    Use pods

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dash',
+    podModulePrefix: 'dash/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
